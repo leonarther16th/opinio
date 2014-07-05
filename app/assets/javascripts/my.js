d5d25023602a1035f78226c2197c47163fc6d4c6
Moe Alkhatib <leonarther16th@gmail.com>
2014-07-05 12:17:29 -0400

    further inhancment js"

--- app/assets/javascripts/my.js
+++ app/assets/javascripts/my.js
@@ -1,5 +1,5 @@
 //$(".post-action").toggleClass("hider");
-$(".btn-group").toggleClass("hider");
+var post_func = $(".btn-group").toggleClass("hider");
 $(".well").hover(function(){
 	$(this).addClass("focus");
 	//$(this).children(".post-action").toggleClass("hider");
@@ -18,7 +18,12 @@ var counter = 0;
 
 $(".btn-primary").click(function(){
 	counter += 1;
-	$(this).html(counter);
+	//$(this).html(counter);
+	$(this).attr('disabled','disabled').append(" <span class='glyphicon glyphicon-ok'></span> <span class='badge pull-right'>70%</span>").addClass('btn-success');
+	$(this).prev().attr('disabled','disabled').append(" <span class='glyphicon glyphicon-remove'></span> <span class='badge pull-right'>65%</span>").addClass('btn-danger');
+	$(this).next().attr('disabled','disabled').append(" <span class='glyphicon glyphicon-remove'></span> <span class='badge pull-right'>31%</span>").addClass('btn-danger');
+	$(this).prev().prev().attr('disabled','disabled').append(" <span class='glyphicon glyphicon-remove'></span> <span class='badge pull-right'>42%</span>").addClass('btn-danger');
+	$(this).next().next().attr('disabled','disabled').append(" <span class='glyphicon glyphicon-remove'></span> <span class='badge pull-right'>21%</span>").addClass('btn-danger');
 });
 
 $("#new-post").addClass('hider');
