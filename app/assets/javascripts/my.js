8561a8b30186a99fca9a91664a8bb1d3c5929401
Moe Alkhatib <leonarther16th@gmail.com>
2014-07-08 00:13:03 -0400

    Added char counter to fields

--- app/assets/javascripts/my.js
+++ app/assets/javascripts/my.js
@@ -34,3 +34,30 @@ $("#new-post-btn").click(function(){
 });
 
 $(".test").removeClass("hider");
+
+
+$(function(){
+$("#statement_textarea").characterCounter({
+limit: 150, // characters limit
+counterSelector: false, // allow one or more counters to be specified by a jQuery selector
+counterWrapper: 'span', // the element you wish to wrap your counter in.
+counterCssClass: 'text-counters', // the CSS class to apply to your counter.
+counterFormat: 'Characters Remaining: %1', // the format of your counter text where '%1' will be replaced with the remaining character count.
+counterExceededCssClass: 'text-counters-error', // the CSS class to apply when your limit has been exceeded.
+onExceed: function(count) {},
+onDeceed: function(count) {},
+customFields: {},}); // key value pairs of custom options to be added to the counter such as class, data attributes etc.
+});
+
+$(function(){
+$(".option_field").characterCounter({
+limit: 20, // characters limit
+counterSelector: false, // allow one or more counters to be specified by a jQuery selector
+counterWrapper: 'span', // the element you wish to wrap your counter in.
+counterCssClass: 'text-counters', // the CSS class to apply to your counter.
+counterFormat: 'Characters Remaining: %1', // the format of your counter text where '%1' will be replaced with the remaining character count.
+counterExceededCssClass: 'text-counters-error', // the CSS class to apply when your limit has been exceeded.
+onExceed: function(count) {},
+onDeceed: function(count) {},
+customFields: {},}); // key value pairs of custom options to be added to the counter such as class, data attributes etc.
+});
